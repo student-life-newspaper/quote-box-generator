148b269a7f7f1c914e39afb655edd16fc5bd39ad
Nathan Springman <naspringman@gmail.com>
2021-02-28 14:23:25 -0600

    feat: add more fields to qb form

--- generator/forms.py
+++ generator/forms.py
@@ -2,3 +2,8 @@ from django import forms
 
 class QuoteBoxForm(forms.Form):
     quote_text = forms.CharField(label='Quote text', max_length=300)
+    quote_citation = forms.CharField(label='Quote citation', max_length=200)
+    background_color = forms.CharField(label='Background color (6 character hex value)', initial="e3070e", max_length=6)
+    text_color = forms.CharField(label='Text color (6 character hex value)', initial="ffffff", max_length=6)
+    width = forms.IntegerField(label="Width (px)", initial=1900)
+    height = forms.IntegerField(label="Height (px)", initial=1200)
