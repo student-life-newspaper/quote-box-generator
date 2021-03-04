ba83ab28c09f8e9412c6edfa7d6e4384b0dc7a79
Nathan Springman <naspringman@gmail.com>
2021-03-04 13:19:37 -0600

    style: add very basic css

--- generator/forms.py
+++ generator/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class QuoteBoxForm(forms.Form):
-    quote_text = forms.CharField(label='Quote text', max_length=300)
+    quote_text = forms.CharField(label='Quote text', max_length=500, widget=forms.Textarea)
     quote_citation = forms.CharField(label='Quote citation', max_length=200)
     background_color = forms.CharField(label='Background color (6 character hex value)', initial="e3070e", max_length=6)
     text_color = forms.CharField(label='Text color (6 character hex value)', initial="ffffff", max_length=6)
