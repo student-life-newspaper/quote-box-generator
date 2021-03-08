e94b524fcf19f9e2e4af0c15a778d8531a650804
Nathan Springman <naspringman@gmail.com>
2021-03-08 13:28:29 -0600

    feat: add radio select for background color

--- generator/forms.py
+++ generator/forms.py
@@ -14,10 +14,19 @@ class HexFormField(forms.CharField):
             raise forms.ValidationError(self.error_messages['invalid'])
         return value
 
+BACKGROUND_COLOR_CHOICES = [
+    ('b1170f','Red'),
+    ('696969','Grey'),
+    ('007d2c','Green'),
+    ('0e1e5c','Blue'),
+    ('b1170f','Custom'),
+]
+
 class QuoteBoxForm(forms.Form):
     quote_text = forms.CharField(label='Quote text', max_length=500, widget=forms.Textarea)
     quote_citation = forms.CharField(label='Quote citation', max_length=200)
-    background_color = HexFormField(label='Background color (6 character hex value)', initial="e3070e", max_length=6)
+    background_color = forms.ChoiceField(label="Background color", widget=forms.RadioSelect, choices=BACKGROUND_COLOR_CHOICES)
+    background_color_custom = HexFormField(label='Custom background color (6 character hex value)', initial="", max_length=6, required=False)
     text_color = HexFormField(label='Text color (6 character hex value)', initial="ffffff", max_length=6)
     width = forms.IntegerField(label="Width (px)", initial=1116)
     height = forms.IntegerField(label="Height (px)", initial=706)
