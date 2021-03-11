0a63a80fa85704f5f35182faea3994b09fed14be
Nathan Springman <naspringman@gmail.com>
2021-03-10 22:12:47 -0600

    feat: catch exceptions when generating graphic

--- generator/views.py
+++ generator/views.py
@@ -40,76 +40,82 @@ def generate_quote_box(request):
     if request.method == "GET":
         form = QuoteBoxForm(request.GET)
         if form.is_valid():
-            qb = form.cleaned_data
+            try:
+                qb = form.cleaned_data
 
-            if qb['background_color'] == 'Custom':
-                form_background_color = qb['background_color_custom']
-            else:
-                form_background_color = qb['background_color']
-            background_color = ImageColor.getrgb('#' + form_background_color)
+                if qb['background_color'] == 'Custom':
+                    form_background_color = qb['background_color_custom']
+                else:
+                    form_background_color = qb['background_color']
+                background_color = ImageColor.getrgb('#' + form_background_color)
 
-            if qb['text_color'] == 'Custom':
-                text_color = ImageColor.getrgb('#' + qb['text_color_custom'])
-            else:
-                text_color = ImageColor.getrgb('#' + qb['text_color'])
+                if qb['text_color'] == 'Custom':
+                    text_color = ImageColor.getrgb('#' + qb['text_color_custom'])
+                else:
+                    text_color = ImageColor.getrgb('#' + qb['text_color'])
 
-            imageWidth = qb['width']
-            imageHeight = qb['height']
+                imageWidth = qb['width']
+                imageHeight = qb['height']
 
-            im = Image.new("RGB", (imageWidth,imageHeight), color=background_color)
-            draw = ImageDraw.Draw(im)
+                im = Image.new("RGB", (imageWidth,imageHeight), color=background_color)
+                draw = ImageDraw.Draw(im)
 
-            # define bars
-            barMargin = (int)(imageWidth / 27)
-            barWeight = (int)(imageWidth / 43)
-            barHorizontalLength = imageWidth * 0.4
-            barVerticalLength = imageHeight * 0.35
-            barColor = text_color
+                # define bars
+                barMargin = (int)(imageWidth / 27)
+                barWeight = (int)(imageWidth / 43)
+                barHorizontalLength = imageWidth * 0.4
+                barVerticalLength = imageHeight * 0.35
+                barColor = text_color
 
-            # draw upper bars
-            draw.rectangle([barMargin, barMargin, barHorizontalLength, barMargin + barWeight], fill=barColor)
-            draw.rectangle([barMargin, barMargin, barMargin + barWeight, barVerticalLength], fill=barColor)
-            # draw lower bars
-            draw.rectangle([imageWidth - barMargin - barHorizontalLength, imageHeight - barMargin - barWeight, imageWidth - barMargin, imageHeight - barMargin], fill=barColor)
-            draw.rectangle([imageWidth - barMargin, imageHeight - barMargin, imageWidth - barMargin - barWeight, imageHeight - barWeight - barVerticalLength], fill=barColor)
+                # draw upper bars
+                draw.rectangle([barMargin, barMargin, barHorizontalLength, barMargin + barWeight], fill=barColor)
+                draw.rectangle([barMargin, barMargin, barMargin + barWeight, barVerticalLength], fill=barColor)
+                # draw lower bars
+                draw.rectangle([imageWidth - barMargin - barHorizontalLength, imageHeight - barMargin - barWeight, imageWidth - barMargin, imageHeight - barMargin], fill=barColor)
+                draw.rectangle([imageWidth - barMargin, imageHeight - barMargin, imageWidth - barMargin - barWeight, imageHeight - barWeight - barVerticalLength], fill=barColor)
 
-            
-            text = qb['quote_text']
-            # get lines to wrap text
-            if(qb['quote_font_size'] == 'Auto'):
-                quote_lines, quote_font, quote_font_size, line_height = find_quote_font_size(text, (imageWidth - 6 * barMargin), (imageHeight * 0.6))
-            else:
-                quote_font_size = qb['quote_font_size_custom']
-                quote_font = ImageFont.truetype("/app/fonts/Georgia.ttf", size=quote_font_size)
-                quote_lines = text_wrap(text, quote_font, (imageWidth - 6 * barMargin))
-                line_height = quote_font.getsize('hg')[1]
+                
+                text = qb['quote_text']
+                # get lines to wrap text
+                if(qb['quote_font_size'] == 'Auto'):
+                    quote_lines, quote_font, quote_font_size, line_height = find_quote_font_size(text, (imageWidth - 6 * barMargin), (imageHeight * 0.6))
+                else:
+                    quote_font_size = qb['quote_font_size_custom']
+                    quote_font = ImageFont.truetype("/app/fonts/Georgia.ttf", size=quote_font_size)
+                    quote_lines = text_wrap(text, quote_font, (imageWidth - 6 * barMargin))
+                    line_height = quote_font.getsize('hg')[1]
 
-            # quote positioning
-            quote_x = 2 * barMargin + barWeight
-            quote_y = 1.75 * barMargin + barWeight
+                # quote positioning
+                quote_x = 2 * barMargin + barWeight
+                quote_y = 1.75 * barMargin + barWeight
 
-            # print quote lines
-            for line in quote_lines:
-                draw.text((quote_x,quote_y), line, fill=text_color, font=quote_font)
-                quote_y = quote_y + line_height
+                # print quote lines
+                for line in quote_lines:
+                    draw.text((quote_x,quote_y), line, fill=text_color, font=quote_font)
+                    quote_y = quote_y + line_height
 
-            # set citation
-            quote_citation = "— " + qb['quote_citation']
+                # set citation
+                quote_citation = "— " + qb['quote_citation']
 
-            # calculate citation width
-            if(qb['citation_font_size'] == 'Auto'):
-                citation_font = ImageFont.truetype("/app/fonts/Georgia.ttf", size=50)
-                target_size = imageWidth * 0.6
-                current_citation_width = citation_font.getsize(quote_citation)[0]
-                new_citation_font_size = (target_size / current_citation_width) * 50
-                citation_font = ImageFont.truetype("/app/fonts/Georgia.ttf", size=int(new_citation_font_size))
-            else:
-                citation_font = ImageFont.truetype("/app/fonts/Georgia.ttf", size=quote_font_size)
-            quote_citation_size = citation_font.getsize(quote_citation)
-            quote_citation_position = ((imageWidth - (3 * barMargin) - quote_citation_size[0]), 
-                                        (imageHeight - (2.5 * barMargin) - quote_citation_size[1]))
+                # calculate citation width
+                if(qb['citation_font_size'] == 'Auto'):
+                    citation_font = ImageFont.truetype("/app/fonts/Georgia.ttf", size=50)
+                    target_size = imageWidth * 0.6
+                    current_citation_width = citation_font.getsize(quote_citation)[0]
+                    new_citation_font_size = (target_size / current_citation_width) * 50
+                    citation_font = ImageFont.truetype("/app/fonts/Georgia.ttf", size=int(new_citation_font_size))
+                else:
+                    citation_font = ImageFont.truetype("/app/fonts/Georgia.ttf", size=quote_font_size)
+                quote_citation_size = citation_font.getsize(quote_citation)
+                quote_citation_position = ((imageWidth - (3 * barMargin) - quote_citation_size[0]), 
+                                            (imageHeight - (2.5 * barMargin) - quote_citation_size[1]))
 
-            draw.text((quote_citation_position[0],quote_citation_position[1]), quote_citation, fill=text_color, font=citation_font)
+                draw.text((quote_citation_position[0],quote_citation_position[1]), quote_citation, fill=text_color, font=citation_font)
+            except:
+                im = Image.new("RGB", (600,200), color=(227, 7, 14))
+                draw = ImageDraw.Draw(im)
+                quote_font = ImageFont.truetype("/app/fonts/Georgia.ttf", size=40)
+                draw.text((50,50), 'An error has occured\nPlease check form values', fill=(255,255,255), font=quote_font)
         else:
             im = Image.new("RGB", (600,200), color=(227, 7, 14))
             draw = ImageDraw.Draw(im)
